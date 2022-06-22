774d89a09ad2647dd6324f7d6bfdc33271052fdc
huynguyen121298 <huy.nguyen@s3corp.com.vn>
2022-06-22 11:14:10 +0700

    update source

--- UI/obj/Release/Package/PackageTmp/js/JavaScript.js
+++
@@ -1,9 +0,0 @@
-﻿function ShowImagePreview(imageUploader, PreviewImage) {
-    if (imageUploader.flle && imageUploader.flle[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $(PreviewImage).attr('src', e.target.result);
-        }
-        reader.readAsDataURL(imageUploader.flle[0]);
-    }
-}
